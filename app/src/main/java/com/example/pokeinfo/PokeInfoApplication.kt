c8f7cee4bdfd8ee1c01a48ccb242c3a75a379baf
woosang <woosang2918@gmail.com>
2023-09-17 14:20:48 +0900

    feat : 리소스 추가

--- app/src/main/java/com/example/pokeinfo/PokeInfoApplication.kt
+++ app/src/main/java/com/example/pokeinfo/PokeInfoApplication.kt
@@ -2,21 +2,9 @@ package com.example.pokeinfo
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class PokeInfoApplication : Application() {
 
-    companion object {
-        private lateinit var instance: PokeInfoApplication
-
-        private fun setInstance(application: PokeInfoApplication) {
-            instance = application
-        }
-
-        fun getGlobalContext(): Context = instance.applicationContext
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        setInstance(this@PokeInfoApplication)
-    }
 }
